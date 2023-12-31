34386f9ccdeb8e65981baf5a3ab5c68f98f6593c
Maxime Le Duc <maximeld19@gmail.com>
2023-12-31 19:52:07 +0100

    remove not use imports

--- libs/inventory/src/inventory.module.ts
+++ libs/inventory/src/inventory.module.ts
@@ -1,13 +1,9 @@
-import { Module, Scope } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGateway } from '@app/authentication/hexagon/gateways/auth.gateway';
-import { AuthGuard } from '@app/inventory/write/infra/clients/nestjs/guards/auth.guard';
+import { Module } from '@nestjs/common';
 import { WriteModule } from '@app/inventory/write';
 import { ReadModule } from '@app/inventory/read';
-import { WriteGatewaysModule } from '@app/inventory/write/infra/clients/nestjs/write-gateways.module';
 
 @Module({
-  imports: [WriteModule, ReadModule, WriteGatewaysModule], // TODO move WriteGatewaysModule to auth context
+  imports: [WriteModule, ReadModule],
   providers: [],
 })
 export class InventoryModule {}
