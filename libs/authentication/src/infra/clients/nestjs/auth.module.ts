ef475606fae5614690cd5b8410d21a9b7a0ed724
Maxime Le Duc <maximeld19@gmail.com>
2024-01-06 23:47:35 +0100

    Move Guard to auth lib

--- libs/authentication/src/infra/clients/nestjs/auth.module.ts
+++ libs/authentication/src/infra/clients/nestjs/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module, Scope } from '@nestjs/common';
 import { AuthController } from '@app/authentication/infra/clients/nestjs/controllers/auth.controller';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@app/inventory/write/infra/clients/nestjs/guards/auth.guard';
+import { AuthGuard } from '@app/authentication/infra/clients/nestjs/guards/auth.guard';
 import { AuthGatewaysModule } from '@app/authentication/infra/clients/nestjs/auth-gateways.module';
 import { AuthGateway } from '@app/authentication/hexagon/gateways/auth.gateway';
 
