486bbec5236c5167f70f150ac422d81756ad09a6
Maxime Le Duc <maximeleduc@unknown.home>
2024-01-29 18:32:49 +0100

    Use query params

--- libs/inventory/src/read/infra/clients/nestjs/controllers/read-folders.controller.ts
+++ libs/inventory/src/read/infra/clients/nestjs/controllers/read-folders.controller.ts
@@ -1,19 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { GetItemsInFolderUseCase } from '@app/inventory/read/hexagon/usecases/get-items-in-folder/get-items-in-folder.usecase';
+import { Controller } from '@nestjs/common';
 
 @Controller('folders')
 export class ReadFoldersController {
-  constructor(
-    private readonly getItemsInFolderUseCase: GetItemsInFolderUseCase,
-  ) {}
-
-  @Get(':id/items')
-  getItemsInFolder(@Param('id') id: string) {
-    return this.getItemsInFolderUseCase.execute({ folderId: id });
-  }
-
-  @Get('/items')
-  getItemsInRootFolder() {
-    return this.getItemsInFolderUseCase.execute({});
-  }
+  constructor() {}
 }
--- libs/inventory/src/read/infra/clients/nestjs/controllers/read-items.controller.ts
+++ libs/inventory/src/read/infra/clients/nestjs/controllers/read-items.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { GetItemByIdUseCase } from '@app/inventory/read/hexagon/usecases/get-item-by-id/get-item-by-id.usecase';
+import { GetFoldersQuery } from '@app/inventory/read/infra/clients/nestjs/queries/get-folders.query';
+import { GetItemsInFolderUseCase } from '@app/inventory/read/hexagon/usecases/get-items-in-folder/get-items-in-folder.usecase';
 
 @Controller('items')
 export class ReadItemsController {
-  constructor(private readonly getItemByIdUseCase: GetItemByIdUseCase) {}
+  constructor(
+    private readonly getItemByIdUseCase: GetItemByIdUseCase,
+    private readonly getItemsInFolderUseCase: GetItemsInFolderUseCase,
+  ) {}
   @Get(':id')
   getItemById(@Param('id') id: string) {
     return this.getItemByIdUseCase.execute({ id });
+  }
+
+  @Get()
+  getItems(@Query() query: GetFoldersQuery) {
+    return this.getItemsInFolderUseCase.execute({ folderId: query.folderId });
   }
 }
